a319d95de64f69815c8c7108b208a8e9a08e358c
Clloy7 <Cleya0631@gmail.com>
2025-05-02 03:10:45 +0000

    Readme atualizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,53 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char nome1[50], nome2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos_turisticos1, pontos_turisticos2;
+   
+    // Entrada de dados
+    printf("Digite os dados da cidade 1:\n");
+    printf("Nome: ");
+    scanf("%s", nome1);
+    printf("População: ");
+    scanf("%d", &populacao1);
+    printf("Área: ");
+    scanf("%f", &area1);
+    printf("PIB: ");
+    scanf("%f", &pib1);
+    printf("Pontos turísticos: ");
+    scanf("%d", &pontos_turisticos1);
+   
+    printf("Digite os dados da cidade 2:\n");
+    printf("Nome: ");
+    scanf("%s", nome2);
+    printf("População: ");
+    scanf("%d", &populacao2);
+    printf("Área: ");
+    scanf("%f", &area2);
+    printf("PIB: ");
+    scanf("%f", &pib2);
+    printf("Pontos turísticos: ");
+    scanf("%d", &pontos_turisticos2);
+   
+    // Cálculos
+    float densidade_populacional1 = (float)populacao1 / area1;
+    float pib_per_capita1 = pib1 / populacao1;
+    float super_poder1 = populacao1 + area1 + pib1 + pib_per_capita1 + (1 / densidade_populacional1) + pontos_turisticos1;
+   
+    float densidade_populacional2 = (float)populacao2 / area2;
+    float pib_per_capita2 = pib2 / populacao2;
+    float super_poder2 = populacao2 + area2 + pib2 + pib_per_capita2 + (1 / densidade_populacional2) + pontos_turisticos2;
+   
+    // Comparação
+    printf("Comparação de cidades:\n");
+    printf("População: Cidade %s venceu\n", populacao1 > populacao2 ? nome1 : nome2);
+    printf("Área: Cidade %s venceu\n", area1 > area2 ? nome1 : nome2);
+    printf("PIB: Cidade %s venceu\n", pib1 > pib2 ? nome1 : nome2);
+    printf("Pontos turísticos: Cidade %s venceu\n", pontos_turisticos1 > pontos_turisticos2 ? nome1 : nome2);
+    printf("Super poder: Cidade %s venceu\n", super_poder1 > super_poder2 ? nome1 : nome2);
 
     return 0;
 }
---
+++ readme.md
@@ -0,0 +1,21 @@
+#Desafio das cartas-Super-Trunfo-Países
+
+// Entrada de dados: 
+O programa pede ao usuário que insira os dados de duas cidades, incluindo nome, população, área, PIB e pontos turísticos.
+
+// Cálculos:
+ O programa calcula a densidade populacional, PIB per capita e "super poder" para cada cidade.
+
+// Comparação:
+ O programa compara os dados das duas cidades e determina qual cidade vence em cada categoria (população, área, PIB, pontos turísticos e super poder).
+
+// Saída:
+ O programa imprime os resultados da comparação, mostrando qual cidade venceu em cada categoria.
+
+** Funcionalidades
+
+- O programa usa variáveis para armazenar os dados das cidades e os resultados dos cálculos.
+- O programa usa operadores lógicos e aritméticos para realizar os cálculos e comparações.
+- O programa imprime os resultados da comparação usando printf.
+
+O programa pede ao usuário que insira os dados de duas cidades, incluindo nome, população, área, PIB e pontos turísticos. Em seguida, o programa calcula a densidade populacional, PIB per capita e "super poder" para cada cidade e compara os resultados, determinando qual cidade vence em cada categoria. Por fim, o programa imprime os resultados da comparação.
